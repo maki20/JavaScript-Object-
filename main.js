da61a73d47a5fd05c66d3dd3c5499fa10706d335
maki20 <makiselecao@gmail.com>
2020-09-27 22:36:11 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+{
+  const scores = [80,90,40,70];
+  scores.splice(1,1,40,50);
+
+  for(let i = 0; i < scores.length; i++){
+      console.log(`Score ${i}: ${scores[i]}`);
+  }
+
+
+}
