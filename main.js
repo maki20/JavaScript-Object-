ee4654bb75c8830769a1adb81fffb46fc9b700db
maki20 <makiselecao@gmail.com>
2020-09-30 22:49:10 -0400

    New JavaScript page

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 'use strict';
 
 {
+ 
   const point = {
    x: 100,
    y: 180,
   };
+ 
+//   const keys = Object.keys(point); //オブジェクト内のプロパティを列挙
+//   keys.forEach(key => {
+// 　　　console.log(`Key: ${key} Value: ${point[key]}`);
 
-  point.x = 80;
-  point['x'] = 80;
+//   });
 
-  console.log(point.x);
-  console.log(point['y']); 
+  const points = [
+   {x:30, y:20},
+   {x:10, y:50},
+   {x:40, y:40},
+  ];
+
+  console.log(points[1].y);
+
 }
