39d2a6397abb3fd58c3c1a9751acb101def62992
maki20 <makiselecao@gmail.com>
2020-10-01 23:39:17 -0400

    lesson 16 done

--- main.js
+++ main.js
@@ -1,24 +1,11 @@
 'use strict';
 
+
 {
- 
-  const point = {
-   x: 100,
-   y: 180,
-  };
- 
-//   const keys = Object.keys(point); //オブジェクト内のプロパティを列挙
-//   keys.forEach(key => {
-// 　　　console.log(`Key: ${key} Value: ${point[key]}`);
-
-//   });
-
-  const points = [
-   {x:30, y:20},
-   {x:10, y:50},
-   {x:40, y:40},
-  ];
-
-  console.log(points[1].y);
+  let x = [1,2];
+  let y = [...x];  // スプリット構文を使えば
+  x [0]= 5;
+  console.log(x); // [5,2]
+  console.log(y); // [1,2] 
 
 }
